178be55512fb3c7a090e33be83c87c313a4f7b02
William Barela <william.barela.dev@gmail.com>
2020-01-23 07:43:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Your code goes here
 
 let idUpdate = function(){
-  document.getElementById("text").innerHTML = "Javascript is so cool";
+  document.getElementById("text").innerHTML = "Javascript is so cool. It lets me add text to my page programmatically.";
 }
 
 document.addEventListener("DOMContentLoaded",
